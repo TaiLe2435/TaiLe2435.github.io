65bbe7ccb981fe06fd4e13deb8a6e4c61f5187ad
Tommy Le <TaiLe2435@gmail.com>
2024-01-08 16:22:31 -0600

    edited about me section

--- data.js
+++ data.js
@@ -1,6 +1,8 @@
 export const bio = [
     "Welcome to my project portfolio!",
-    `I am a roboticist with experience in robot kinematics/dynamics, control theory, mechatronics, and embedded software development.`,
+    `I have attended Northern Illinois University since 2019 where I graduated with a Bachelor's of Science in Mechatronics Engineering and am currently working 
+    towards my Master's in Mechanical Engineering. During my time at NIU, I have participated in the development of autonomous robots.`
+    `From my experiences I have become a roboticist with experience in robot kinematics/dynamics, control theory, mechatronics, and embedded software development.`,
     `Much of my work has involved the development and implementation of algorithms onto robotic systems. Examples of problems I have worked on deal with 
     state estimation and control, path planning, mechatronic system integration, robotic hardware and software development, automation, and artificial intelligence.`,
     "Thanks for taking the time to learn a little about me!",
