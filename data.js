fbe27bbf83f18ad3ea5cb7c443400b281d97aa1f
Tommy Le <TaiLe2435@gmail.com>
2024-01-08 16:30:37 -0600

    fixed typo in footer

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 export const bio = [
     "Welcome to my project portfolio!",
     `I have attended Northern Illinois University since 2019 where I graduated with a Bachelor's of Science in Mechatronics Engineering and am currently working 
-    towards my Master's in Mechanical Engineering. During my time at NIU, I have participated in the development of autonomous robots.`,
-    `From my experiences I have become a roboticist with experience in robot kinematics/dynamics, control theory, mechatronics, and embedded software development.`,
+    towards my Master's in Mechanical Engineering. During my time at NIU, I have participated in the development of autonomous robots in Dr. Ji-Chul Ryu's Autonomous Robotic Manipulation Laboratory.`,
+    `From my experiences I have developed skills in robot kinematics/dynamics, control theory, mechatronics, and embedded software development.`,
     `Much of my work has involved the development and implementation of algorithms onto robotic systems. Examples of problems I have worked on deal with 
     state estimation and control, path planning, mechatronic system integration, robotic hardware and software development, automation, and artificial intelligence.`,
     "Thanks for taking the time to learn a little about me!",
@@ -215,7 +215,7 @@ export const footer = [
         label: "Social Profiles",
         data: [
             {
-                text: "Linkedin",
+                text: "LinkedIn",
                 link: "https://www.linkedin.com/in/tommy-le24//",
             },
         ],
