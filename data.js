034ab698df12b0833a7ef342e99b6d2b3d7963bb
Tommy Le <TaiLe2435@gmail.com>
2024-01-15 14:57:14 -0600

    added headshot

--- data.js
+++ data.js
@@ -1,9 +1,13 @@
 export const bio = [
-    `I have attended Northern Illinois University since 2019 where I graduated with a Bachelor's of Science in Mechatronics Engineering and am currently working 
-    towards my Master's in Mechanical Engineering. During my time at NIU, I have participated in the development of autonomous robots in Dr. Ji-Chul Ryu's Autonomous Robotic Manipulation Laboratory.`,
-    `From my experiences I have developed skills in robot kinematics/dynamics, control theory, mechatronics, and embedded software development.`,
-    `Much of my work has involved the development and implementation of algorithms onto robotic systems. Examples of problems I have worked on deal with 
-    state estimation and control, path planning, mechatronic system integration, robotic hardware and software development, automation, and artificial intelligence.`,
+    `I have attended Northern Illinois University since 2019 where I graduated with a Bachelor's of Science in Mechatronics Engineering and am \ 
+    currently working towards my Master's in Mechanical Engineering. During my time at NIU, I have participated in the development of autonomous \
+    robots in Dr. Ji-Chul Ryu's Autonomous Robotic Manipulation Laboratory.`,
+    `Much of my work has involved the development and implementation of algorithms onto robotic systems. Examples of problems I have worked on \ 
+    deal with state estimation and control, path planning, mechatronic system integration, robotic hardware and software development, \ 
+    automation, and artificial intelligence.`,
+    `From my experiences I have developed skills in robot kinematics/dynamics, control theory, mechatronics, and embedded software development \ 
+    and upon graduation am looking to start a full-time position working on robotics hardware and software development dealing with manipulation \
+    and navigation.`,
 ];
 
 export const skills = [
