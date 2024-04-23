1711fb99ccb1084255a5f58d97f9b719f00d197f
Tommy Le <TaiLe2435@gmail.com>
2024-04-22 20:48:30 -0500

    added links to new papers for 627 and 623

--- data.js
+++ data.js
@@ -176,7 +176,7 @@ export const projects = {
             image: "images/tensorflow.png",
             summary:
                 "Trained a model to detect people, cars, and buses in traffic leading to an average 80% confidence level.",
-            preview: "https://drive.google.com/file/d/1IXisRFXh-cRFcpPsCHbR0mK1jqDhzApK/view?usp=sharing", // find final paper and link it 
+            preview: "https://drive.google.com/file/d/1h01HttsckU9dhd4sQauN8z2ukprFOamh/view?usp=sharing", // find final paper and link it 
             techStack: ["TensorFlow", "Python", "Jupyter Notebook", "OpenCV"],
         },
     ],
